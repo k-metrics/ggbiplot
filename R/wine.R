d5f341f596f3625829fe3700d7d445309d8574a3
k-metrics <24760577+k-metrics@users.noreply.github.com>
2019-03-14 14:00:35 +0900

    Update DESCRIPTION and help, and re-build

---
+++ R/wine.R
@@ -0,0 +1,24 @@
+#' Chemical analysis of wines in Italy
+#' 
+#' @title Wine dataset
+#' @name wine
+#' @aliases wine.class
+#' @description 
+#' Results of a chemical analysis of wines grown in the same region in Italy 
+#' but derived from three different cultivars. The analysis determined the 
+#' quantities of 13 constituents found in each of the three types of wines. 
+#' The cultivars, 'barolo', 'barbera', and 'grignolino', are indicated in wine.class.
+#' @usage data(wine)
+#' 
+#' @docType data
+#' @format 
+#' A \code{wine} data frame consisting of 178 observations (rows) and
+#' 13 columns and vector \code{wine.class} of factors indicating the cultivars.
+#' @source UCI Machine Learning Repository (\url{http://archive.ics.uci.edu/ml/datasets/Wine})
+#' @examples 
+#' data(wine)
+#' wine.pca <- prcomp(wine, scale. = TRUE)
+#' ggscreeplot(wine.pca)
+#' ggbiplot(wine.pca, obs.scale = 1, var.scale = 1, groups = wine.class, ellipse = TRUE, circle = TRUE)
+#'
+"wine"
